d9f50b2848ce25882c34b3a1095a70bc171cc165
Iago Cavalcante <iagoangelimc@gmail.com>
2020-02-14 02:06:56 -0300

    fix: consertando porta como variavel de ambiente

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: process.env.PORT || 5000,
+        host: '0.0.0.0',
         routes: {
           cors: {
             origin: ['*']
